5a429c5e1855206576a2ab0309488b785bf45eda
Matilde Rocha <matilde.rocha@unosquare.com>
2019-07-18 11:18:12 -0500

    Improving

--- src/main/java/com/verisk/g2/take_home_test/api/TravelAPI.java
+++ src/main/java/com/verisk/g2/take_home_test/api/TravelAPI.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin.javascript.navig.Array;
 
 import java.util.ArrayList;
 import java.util.List;
